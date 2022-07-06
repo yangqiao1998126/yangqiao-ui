7c7668f38517dadd59b20651ce2cbb8e64d812a8
yangqiao <yangqiao@megarobo.tech>
2022-07-06 11:16:30 +0800

    entry config

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,54 @@
-import { defineConfig } from "vite";
+import { defineConfig,loadEnv } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import Markdown from "vite-plugin-md";
 import { resolve } from "path";
-export default defineConfig({
-  plugins: [
-    Vue({
-      include: [/\.vue$/, /\.md$/], // <--
-    }),
-    Markdown(),
-  ],
-  build: {
-    rollupOptions: {
-      // 请确保外部化那些你的库中不需要的依赖
-      external: ["vue"],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: "Vue",
+
+
+export default defineConfig(({command,mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const entryName = env.VITE_BASE_ENTRYNAME
+  let buildOptions = {}
+  if(entryName === 'html'){
+
+  }else{
+    buildOptions = {
+      rollupOptions: {
+        // 请确保外部化那些你的库中不需要的依赖
+        external: ["vue"],
+        output: {
+          // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+          globals: {
+            vue: "Vue",
+          },
+        },
+      },
+      lib: {
+        entry: "./packages/index.js",
+        name: "yq-ui",
+      },
+    }
+  }
+  return {
+    plugins: [
+      Vue({
+        include: [/\.vue$/, /\.md$/], // <--
+      }),
+      Markdown(),
+    ],
+    build: {
+      ...buildOptions
+    },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname,".","src"),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+
         },
       },
     },
-    lib: {
-      entry: "./packages/index.js",
-      name: "yq-ui",
-    },
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-
-      },
-    },
-  },
+  }
 });
